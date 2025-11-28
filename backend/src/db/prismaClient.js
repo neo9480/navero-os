78d2ebbb9d7b4216ab1c31c677459e77494c68b8
neo9480 <skyline.ds247@gmail.com>
2025-11-28 17:27:44 +0530

    fix bugs

--- backend/src/db/prismaClient.js
+++ backend/src/db/prismaClient.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../prisma/generated/prisma/client";
+import { PrismaClient } from "../prisma/generated/prisma/client/index.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-export default prisma ;
+export default prisma;
