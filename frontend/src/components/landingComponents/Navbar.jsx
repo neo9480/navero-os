728fc299120d5c3aace396c285293b4b111f3e7a
neo9480 <skyline.ds247@gmail.com>
2026-03-12 15:29:35 +0530

    update animations

--- frontend/src/components/landingComponents/Navbar.jsx
+++ frontend/src/components/landingComponents/Navbar.jsx
@@ -8,7 +8,8 @@ function Navbar() {
     { label: "About Us", path: "/about-us" },
     { label: "Features", path: "/features" },
     { label: "Pricing", path: "/pricing" },
-    { label: "Get Started", path: "/sign-in" },
+    { label: "Sign-In", path: "/sign-in" },
+    { label: "Sign-Up", path: "/sign-up" },
   ];
   const [hoveredIndex, setHoveredIndex] = useState(null);
   return (
@@ -19,29 +20,43 @@ function Navbar() {
           src="imgs/dark-navero-removebg.png"
           alt="logo"
         />
-        <Motion.div className={`flex gap-[4vw] pr-[4vw] font-extralight  font-neue_montreal`}>
+        <Motion.div
+          className={`flex gap-[4vw] pr-[4vw] font-extralight  font-neue_montreal`}>
           {navItems.map((item, index) => {
             return (
               <Link
-                className={`relative ${index === 3 && "mr-[10vw]"}`}
+                className={`relative ${index === 3 && "mr-[10vw]"} ${index === 4 && "-mr-[2vw]"} ${index === 5 && "inline-flex items-center justify-center  cursor-pointer font-[NeueMontreal,Roboto,sans-serif] bg-platinum-600 text-space_indigo-100 leading-none font-normal select-none rounded-full overflow-hidden shadow-[inset_0_0_0_1px_transparent] duration-300 ease-[cubic-bezier(.215,.61,.355,1)]"}`}
                 to={item.path}
                 key={index}
                 onMouseEnter={() => setHoveredIndex(index)}
                 onMouseLeave={() => setHoveredIndex(null)}>
-                {item.label.split("").map((char, i) => (
-                  <Motion.span key={i} className="inline-block">
-                    {char}
-                  </Motion.span>
-                ))}
-                <Motion.div
-                  className="absolute left-0 bottom-0 h-[0.2vh] w-full bg-platinum-500 origin-left"
-                  initial={{ scaleX: 0 }}
-                  animate={{
-                    scaleX: hoveredIndex === index ? 1 : 0,
-                    transformOrigin: hoveredIndex === index ? "left" : "right",
-                  }}
-                  transition={{ duration: 0.4, ease: [0.25, 1, 0.5, 1] }}
-                />
+                {index !== 5 ?
+                  <>
+                    {item.label.split("").map((char, i) => (
+                      <Motion.span key={i} className="inline-block">
+                        {char}
+                      </Motion.span>
+                    ))}
+
+                    <Motion.div
+                      className="absolute left-0 bottom-0 h-[0.2vh] w-full bg-platinum-500 origin-left"
+                      initial={{ scaleX: 0 }}
+                      animate={{
+                        scaleX: hoveredIndex === index ? 1 : 0,
+                        transformOrigin:
+                          hoveredIndex === index ? "left" : "right",
+                      }}
+                      transition={{ duration: 0.4, ease: [0.25, 1, 0.5, 1] }}
+                    />
+                  </>
+                : <>
+                    {/* label (visible) and expanding pill background */}
+                    <span className="relative z-10 inline-block px-[0.5vw] py-[0.5vh] select-none ">
+                      {item.label}
+                    </span>
+                    
+                  </>
+                }
               </Link>
             );
           })}
